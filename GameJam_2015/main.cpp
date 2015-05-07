8645154bf45cf3fde217e0ba39a899981a8f7e42
shean-debatty <shean.debatty@etud.infographie-sup.eu>
2015-05-07 20:54:28 +0200

    starting

---
+++ GameJam_2015/main.cpp
@@ -0,0 +1,22 @@
+#include "flib.h"
+#include "flib_vec2.h"
+
+void Initialize()
+{
+
+}
+
+void Update()
+{
+
+}
+
+void Render()
+{
+
+}
+
+void GfxMain(int, char *[])
+{
+	GfxCallbacks(Initialize, Update, Render);
+}
