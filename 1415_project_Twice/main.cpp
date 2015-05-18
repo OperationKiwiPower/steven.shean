450739c184972b485b0d8292826ba477e70df917
styven184 <steven.sternberger@etud.infographie-sup.eu>
2015-05-18 15:02:52 +0200

    turc

--- 1415_project_Twice/main.cpp
+++ 1415_project_Twice/main.cpp
@@ -2,15 +2,19 @@
 #include "flib_vec2.h"
 #include "SC_Sprite-texture-image.h"
 
+const int g_iNumberEnnemy = 50;
+
+
 TSprite g_tPlayer;
-TSprite g_tEnnemi[50];
+TSprite g_tEnnemi[g_iNumberEnnemy];
 
 void Initialize()
 {
 	g_tPlayer.pSprite = SC_CreateSprite(g_tPlayer);
-
-	g_tEnnemi[1].pSprite = SC_CreateSprite(g_tEnnemi[1]);
-
+	for (int i = 0; i < g_iNumberEnnemy; i++)
+	{
+		g_tEnnemi[1].pSprite = SC_CreateSprite(g_tEnnemi[i]);
+	}
 }
 void Update()
 {
@@ -18,6 +22,7 @@ void Update()
 }
 void Render()
 {
+	GfxClear(EGfxColor_Black);
 
 }
 
