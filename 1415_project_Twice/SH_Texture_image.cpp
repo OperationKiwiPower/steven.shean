4392a3e4a2a72e7bf56fe433885cf6768923b968
shean-debatty <shean.debatty@etud.infographie-sup.eu>
2015-05-18 15:22:15 +0200

    poupoush

--- 1415_project_Twice/SH_Texture_image.cpp
+++ 1415_project_Twice/SH_Texture_image.cpp
@@ -1,4 +1,10 @@
 #include "flib.h"
+#include "flib_vec2.h"
 #include "SC_Sprite-texture-image.h"
 
-
+TGfxTexture * Create_Texture(TSprite & tSprite)
+{
+	TGfxImage * pImage = GfxImageLoad("tileset.tga");
+	tSprite.pTexture = GfxTextureCreate(pImage);
+	return tSprite.pTexture;
+}
