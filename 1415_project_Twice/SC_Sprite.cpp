9d1fc58e4a5c3c31b4e01ff02378c239384c1c53
styven184 <steven.sternberger@etud.infographie-sup.eu>
2015-05-18 15:21:55 +0200

    pupucjheckgv

--- 1415_project_Twice/SC_Sprite.cpp
+++ 1415_project_Twice/SC_Sprite.cpp
@@ -2,9 +2,9 @@
 #include "flib_vec2.h"
 #include "SC_Sprite-texture-image.h"
 
-TGfxSprite * SC_CreateSprite(const TSprite & tSprite)
+TGfxSprite * SC_CreateSprite(TSprite & tSprite)
 {
-
+	tSprite.pSprite = GfxSpriteCreate(tSprite.pTexture);
 
 
 	return tSprite.pSprite;
