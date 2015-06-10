d676954860397f633a4ac10fd8f4205e8417f66d
styven184 <steven.sternberger@gmail.com>
2015-06-10 15:41:20 +0200

    1415_commitTime_Timer is ready

--- 1415_ChronoCommit/main.cpp
+++ 1415_ChronoCommit/main.cpp
@@ -8,35 +8,83 @@ struct TTime
 	int iBetween;
 	int iRestTime;
 };
+struct TSegmentTime
+{
+	int iHour;
+	int iMinute;
+	int iSecond;
+};
+struct TDisplay
+{
+	struct TSize
+	{
+		int full;
+		int half;
+	};
+	TSize X;
+	TSize Y;
+};
+
 TTime g_tTime;
-const int iMaxTimeBetween = 3600000;// 1 heure;
+TSegmentTime g_TSgementTime;
+TDisplay g_tDisplay;
+
+int iMaxTimeBetween = 1;// number heure;
 
 int SC_GetTime()
 {
-	int iTime;
-	
+	int iTime;	
 	g_tTime.iNow = GfxTimeGetMilliseconds();
 	g_tTime.iBetween = g_tTime.iNow - g_tTime.iFirst;
 	iTime = iMaxTimeBetween - g_tTime.iBetween;
 	return iTime;
 }
+void SC_SegmentTime(const int iTime)
+{
+	g_TSgementTime.iHour = int(iTime / 3600000.0f);
+	if (g_TSgementTime.iHour < 0)g_TSgementTime.iHour = 0;
+
+	g_TSgementTime.iMinute = int((iTime / 60000.0f) - (g_TSgementTime.iHour * 60.0f));
+	if (g_TSgementTime.iMinute < 0)g_TSgementTime.iMinute = 0;
+
+	g_TSgementTime.iSecond = int((iTime / 1000.0f) - (g_TSgementTime.iHour * 3600.0f) - (g_TSgementTime.iMinute * 60.0f));
+	if (g_TSgementTime.iSecond < 0)g_TSgementTime.iSecond = 0;
+
+	GfxDbgPrintf("time:%i - %ih %im %is\n", iTime, g_TSgementTime.iHour, g_TSgementTime.iMinute, g_TSgementTime.iSecond);
+}
+void SC_GetDisplaySize()
+{
+	g_tDisplay.X.full = GfxGetDisplaySizeX();
+	g_tDisplay.Y.full = GfxGetDisplaySizeY();
+	g_tDisplay.X.half = g_tDisplay.X.full / 2;
+	g_tDisplay.Y.half = g_tDisplay.Y.full / 2;
+}
 
 void Initialize()
 {
+	iMaxTimeBetween *= 3600000;// * 1 heure;
 	g_tTime.iFirst = GfxTimeGetMilliseconds();
 	g_tTime.pTextSprite = GfxTextSpriteCreate();
+	GfxSpriteSetFilteringEnabled(g_tTime.pTextSprite, false);
+	SC_GetDisplaySize();
 
 }
 void Update()
 {
 	g_tTime.iRestTime = SC_GetTime();
+	SC_SegmentTime(g_tTime.iRestTime);
 
-	GfxTextSpritePrintf(g_tTime.pTextSprite, "Il reste : ", g_tTime.iRestTime);
+	GfxTextSpritePrintf(g_tTime.pTextSprite,
+				"Il reste: %i h %i m %i s",
+				int(g_TSgementTime.iHour), 
+				int(g_TSgementTime.iMinute), 
+				int(g_TSgementTime.iSecond));
 
 }
 void Render()
 {
-
+	GfxClear(EGfxColor_Black);
+	GfxTextSpriteRender(g_tTime.pTextSprite, float(g_tDisplay.X.half), float (g_tDisplay.Y.half), EGfxColor_White, 2.0f, true, true);
 }
 
 void GfxMain(int, char *[])
